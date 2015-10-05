b4067cedef318e38055576f5a72505ec7cfea7e9
tsuzukiTest <tsudukik@tsudukik-no-MacBook-Pro.local>
2015-10-05 16:26:48 +0900

    port部分改変

--- server.js
+++ server.js
@@ -55,7 +55,8 @@ var server = http.createServer(function (request, response) {
 	serveStatic(response, cache, absPath);
 });
 
-server.listen(3000, function () {
+var port = process.env.PORT || 3000;
+server.listen(port, function () {
 	console.log("Server listening on port 3000.");
 });
 
