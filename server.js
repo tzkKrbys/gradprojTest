bbf6496f02512a52ae11d466fca6c49a33686f59
tsuzukiTest <tsudukik@tsudukik-no-MacBook-Pro.local>
2015-10-05 16:15:27 +0900

    var cache 追記

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var http = require('http');
 var fs = require('fs');
 var path = require('path');
 var mime = require('mime');
-
+var cache = {};
 
 function send404(response) {
 	response.writeHead(404, {
